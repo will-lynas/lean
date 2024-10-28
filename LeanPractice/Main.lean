cc38f3e3069c0021ab23a036c2360b9160107a0f
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2024-10-28 14:53:23 +0000

    add more to 002

--- LeanPractice/Main.lean
+++ LeanPractice/Main.lean
@@ -14,7 +14,29 @@ variable (A: Type)
 variable (P: A -> Prop)
 variable (b: A)
 
+-- 002a
+-- One way to solve this is to use `exact`
+-- `h b` gives `b` to the `∀` so we end up with `P b`
+-- Then we use `exact` because this exactly matches the goal
 example (h: ∀a, P a) : P b := by
   exact h b
+
+-- 002b
+-- Another way is to use `apply`
+-- Here lean works out what it should use for `a`
+example (h: ∀a, P a) : P b := by
+  apply h
+
+  -- This also works:
+  -- apply h a
+
+-- 002c
+-- There's also `specialize`
+-- We give `b` to `h`
+-- Then `exact` to close the goal
+example (h: ∀a, P a) : P b := by
+  specialize h b
+  -- Now h is `P b`
+  exact h
 
 end q002
